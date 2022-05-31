87bf510a296c1789c3fbdce31e83d318c5989fa8
SpecialDude <warithadetayo.awa@gmail.com>
2022-05-30 17:05:21 -0700

    Shell Redirection - -confused_smiley

---
+++ task_performer.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+folder="0x02-shell_redirections"
+cfile="$folder/$1"
+
+echo "#!/bin/bash" > $cfile
+echo $2 >> $cfile
+
+chmod u+x $cfile
+git add .
+git commit -m "Shell Redirection - $1"
+git push
