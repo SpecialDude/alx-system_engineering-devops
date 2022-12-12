d7bf7606a41adb2576663ee626a50dd5dcd8a52a
SpecialDude <warithadetayo.awa@gmail.com>
2022-12-12 12:31:09 +0100

    Task 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # This Conf file creates a file in the tmp folder
 
-file { '/tmp/school'
-  path ==> '/tmp/school',
-  mode ==> '0744',
-  owner ==> 'www-data',
-  group ==> 'www-data',
-  content ==> 'I love Puppet',
+file { '/tmp/school':
+  path => '/tmp/school',
+  mode => '0744',
+  owner => 'www-data',
+  group => 'www-data',
+  content => 'I love Puppet',
 }
